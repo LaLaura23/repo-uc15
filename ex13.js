9cdedfb7d7714f5ae37bcb03043b35e85c5db9dc
LaLaura23 <141740418+LaLaura23@users.noreply.github.com>
2024-11-06 14:42:53 -0300

    Delete ex13.js

--- ex13.js
+++
@@ -1,9 +0,0 @@
-/*
-Exercício 13: Lista de Compras
-Crie um programa que contenha um array com uma lista de compras. 
-O programa deve percorrer o array e mostrar cada item precedido pela mensagem "Preciso comprar: ".
-*/
-
-let compras = ['maçã', 'pizza', 'chocolate', 'coxinha', 'sorvete']
-
-console.log(`Preciso comprar: ${compras}`)
